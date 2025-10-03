fa3a41114febd5a6051c677985f926ad83340cbd
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2025-10-03 00:44:58 -0400

    feat: add home and gallery components with routing
    - Implemented routing for Home and Gallery components in app.routes.ts.
    - Created Header component for navigation with dynamic class based on route.

--- portfolio-site/src/app/app.routes.ts
+++ portfolio-site/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Gallery } from './gallery/gallery';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home', 
+        component: Home
+    },
+    {
+        path: 'gallery',
+        title: 'Gallery',
+        component: Gallery
+    },
+];
---
+++ portfolio-site/src/app/components/header/header.ts
@@ -0,0 +1,23 @@
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink],
+  templateUrl: './header.html',
+  styleUrl: './header.scss'
+})
+export class Header implements OnInit {
+  private router = inject(Router);
+  headerClass = signal(['center-screen', 'left-screen']);
+
+  ngOnInit(): void {
+    // Subscribe to router events to update header class based on current route
+    this.router.events
+      .pipe(filter( event => event instanceof NavigationEnd ))
+      .subscribe(event => { 
+        this.headerClass.set(event.urlAfterRedirects === '/' ? ['center-screen'] : ['left-screen']) 
+      });
+  }
+}
